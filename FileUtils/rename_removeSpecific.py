8e1968ad37839b6d84104eacf3c6e3f546f0a646
marshalchen <cymcsg@gmail.com>
2014-09-02 09:09:31 +0800

    update rename_removeSpecific.py

--- FileUtils/rename_removeSpecific.py
+++ FileUtils/rename_removeSpecific.py
@@ -3,11 +3,12 @@ import sys
 import re
 
 import os, re
-regString="(\w+)@2x.png"
+regString="(\w+).\d.png"
 for item in os.listdir('.'):
-    if (re.match(r"(\w+)@2x.png", item)):
+    if (re.match(regString, item)):
         print item
-        newname = re.sub(r"@2x", "", item)
+        newname = re.sub(r".(\d)", r"_\1", item)
         os.rename(item, newname)
         print "-->" + newname
+        #break
    # print item
